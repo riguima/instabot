0f4db241b64833ec66ab872756b5b0fc9bbe1dab
Richard <richard.alexsander.guima@gmail.com>
2023-09-25 13:34:49 -0300

    fix user not found error in mentions

--- instabot/browser.py
+++ instabot/browser.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from instagrapi import Client
 from instagrapi.types import StoryLink, StoryMention
+from instagrapi.exceptions import UserNotFound
 
 
 class Browser:
@@ -16,17 +17,23 @@ class Browser:
             function = client.photo_upload_to_story
         else:
             function = client.video_upload_to_story
+        story_mentions = []
+        for mention in mentions:
+            try:
+                user = client.user_info_by_username(mention)
+                story_mentions.append(
+                    StoryMention(
+                        user=user,
+                        x=0.1,
+                        y=0.1,
+                        width=0.1,
+                        height=0.1,
+                    )
+                )
+            except UserNotFound:
+                continue
         function(
             str(Path(media_path).absolute()),
-            mentions=[
-                StoryMention(
-                    user=client.user_info_by_username(mention),
-                    x=0.1,
-                    y=0.1,
-                    width=0.1,
-                    height=0.1,
-                )
-                for mention in mentions
-            ],
+            mentions=story_mentions,
             links=[StoryLink(webUri=link, x=0.5, y=0.75) for link in links],
         )
